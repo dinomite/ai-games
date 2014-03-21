6ded6a73fd0aa4a6b1ade632409f32ef734721c5
Drew Stephens <drew@dinomite.net>
2014-03-21 17:25:44 -0400

    Calculate ownership share correctly

--- src/main/java/bot/BotStarter.java
+++ src/main/java/bot/BotStarter.java
@@ -10,8 +10,12 @@ import com.theaigames.warlight.move.PlaceArmiesMove;
 import net.dinomite.theaigames.warlight.SuperRegionOwnershipShareComparator;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class BotStarter implements Bot {
     private final float MIN_ATTACK_RATIO = 3;
@@ -49,42 +53,51 @@ public class BotStarter implements Bot {
         LinkedList<SuperRegion> superRegions = state.getVisibleMap().getSuperRegions();
         Collections.sort(superRegions, new SuperRegionOwnershipShareComparator(myName));
 
-        for (Region region : state.getVisibleMap().getOwnedRegions(myName)) {
-            if (armiesLeft <= 0) {
-                break;
-            }
-
-            // Regions with neutral neighbors
-            LinkedList<Region> neutralNeighbors = region.getNeutralNeighbors();
-            if (neutralNeighbors.size() != 0) {
-                // Give the region enough armies to attack a neutral neighbor
-                int armiesToPlace = armiesNeeded(neutralNeighbors.getFirst()) - region.getArmies() + 1;
-
-                // Don't place zero or negative armies
-                if (armiesToPlace <= 0) {
-                    continue;
-                }
-
-                // Only place available armies
-                if (armiesToPlace > armiesLeft) {
-                    armiesToPlace = armiesLeft;
-                }
-
-                placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesToPlace));
-                armiesLeft = armiesLeft - armiesToPlace;
+        System.err.println("=============== BEGIN GETTING REGION OWNERSHIP ==============");
+        // Make a map of SuperRegions we have a presence in, but don't own
+        SortedMap<Float, SuperRegion> superRegionsByOwnership = new TreeMap<>();
+        for (SuperRegion superRegion : state.getVisibleMap().getSuperRegions()) {
+            float ownershipShare = superRegion.ownershipShare(myName);
+            System.err.println("SuperRegion " + superRegion.getId() + " ownership share: " + ownershipShare);
+            if (!superRegion.ownedByPlayer(myName)) {
+                superRegionsByOwnership.put(ownershipShare, superRegion);
             }
         }
 
+        System.err.println("=============== BEGIN PLACEMENT ==============");
+        List<SuperRegion> values = new ArrayList<>(superRegionsByOwnership.values());
+        Collections.reverse(values);
+        for (SuperRegion superRegion : values) {
+            for (Region region : superRegion.getOwnedRegions(myName)) {
+                if (armiesLeft <= 0) {
+                    break;
+                }
+
+                // Regions with neutral neighbors
+                LinkedList<Region> neutralNeighbors = region.getNeutralNeighbors();
+                if (neutralNeighbors.size() != 0) {
+                    // Give the region enough armies to attack a neutral neighbor
+                    int armiesToPlace = armiesNeeded(neutralNeighbors.getFirst()) - region.getArmies() + 1;
+
+                    // Don't place zero or negative armies
+                    if (armiesToPlace <= 0) {
+                        continue;
+                    }
+
+                    // Only place available armies
+                    if (armiesToPlace > armiesLeft) {
+                        armiesToPlace = armiesLeft;
+                    }
+
+                    System.err.println("Placing " + armiesToPlace + " in region " + region.getId() + " within super region " + superRegion.getId());
+                    placeArmiesMoves.add(new PlaceArmiesMove(myName, region, armiesToPlace));
+                    armiesLeft = armiesLeft - armiesToPlace;
+                }
+            }
+        }
 
         System.err.println("Round " + state.getRoundNumber() + ": " + placeArmiesMoves.size() + " placement moves");
-        for (PlaceArmiesMove move : placeArmiesMoves) {
-            System.err.println(move.getString());
-        }
         return placeArmiesMoves;
-    }
-
-    private int armiesNeeded(Region region) {
-        return (int) Math.ceil((MIN_ATTACK_RATIO * region.getArmies()));
     }
 
     @Override
@@ -93,33 +106,34 @@ public class BotStarter implements Bot {
         String myName = state.getMyPlayerName();
         int armies = 5;
 
-        for (Region fromRegion : state.getVisibleMap().getRegions()) {
-            if (fromRegion.ownedByPlayer(myName)) //do an attack
-            {
-                ArrayList<Region> possibleToRegions = new ArrayList<>();
-                possibleToRegions.addAll(fromRegion.getNeighbors());
+        for (Region fromRegion : state.getVisibleMap().getOwnedRegions(myName)) {
+            ArrayList<Region> possibleToRegions = new ArrayList<>();
+            possibleToRegions.addAll(fromRegion.getNeighbors());
 
-                while (!possibleToRegions.isEmpty()) {
-                    double rand = Math.random();
-                    int r = (int) (rand * possibleToRegions.size());
-                    Region toRegion = possibleToRegions.get(r);
+            while (!possibleToRegions.isEmpty()) {
+                double rand = Math.random();
+                int r = (int) (rand * possibleToRegions.size());
+                Region toRegion = possibleToRegions.get(r);
 
-                    if (!toRegion.getOwner().equals(myName) && fromRegion.getArmies() > 6) //do an attack
-                    {
-                        attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armies));
-                        break;
-                    } else if (toRegion.getOwner().equals(myName) && fromRegion.getArmies() > 1) //do a transfer
-                    {
-                        attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armies));
-                        break;
-                    } else {
-                        possibleToRegions.remove(toRegion);
-                    }
+                if (!toRegion.getOwner().equals(myName) && fromRegion.getArmies() > 6) //do an attack
+                {
+                    attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armies));
+                    break;
+                } else if (toRegion.getOwner().equals(myName) && fromRegion.getArmies() > 1) //do a transfer
+                {
+                    attackTransferMoves.add(new AttackTransferMove(myName, fromRegion, toRegion, armies));
+                    break;
+                } else {
+                    possibleToRegions.remove(toRegion);
                 }
             }
         }
 
         return attackTransferMoves;
+    }
+
+    private int armiesNeeded(Region region) {
+        return (int) Math.ceil((MIN_ATTACK_RATIO * region.getArmies()));
     }
 
     public static void main(String[] args) {
